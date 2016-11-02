229671449c796b27f684ddea0df06df72c62674e
Alan Russell <ajrussellaudio@gmail.com>
2016-11-02 10:10:54 +0000

    refactor osc bank

--- client/src/components/OscBank.jsx
+++ client/src/components/OscBank.jsx
@@ -5,18 +5,30 @@ class OscBank extends React.Component{
   constructor(context) {
     super(context);
     this.context = context;
-    this.oscillators = [ context.createOscillator(), context.createOscillator() ];
+
+    // const oscillators = [ context.createOscillator(), context.createOscillator() ] // CHEATING
+
+    // const oscillators = new Array(2).fill( context.createOscillator() ); // two references to same osc
+
+    const oscillators = Array(null,null).map( () => context.createOscillator() ); // [] length: 2
+
+    this.oscillators = [];
+    this.addOscillators(2);
+  }
+
+  addOscillators(num) {
+    if(num < 1) return;
+    this.oscillators.push( this.context.createOscillator() );
+    this.addOscillators( num - 1 );
   }
 
   setWaveform( waveform ) {
-    this.oscillators.forEach( (osc) => { osc.type = waveform } )
+    this.oscillators.forEach( osc => osc.type = waveform )
   }
 
   setFrequency( freq, time ) {
     if(!time) time = this.context.currentTime;
-    this.oscillators.forEach( (osc) => { 
-      osc.frequency.setValueAtTime(freq, time);
-    });
+    this.oscillators.forEach( osc => osc.frequency.setValueAtTime(freq, time) );
   }
 
   setDetune( cents, time ) {
@@ -25,11 +37,14 @@ class OscBank extends React.Component{
   }
 
   connect( object ) {
-    this.oscillators.forEach( (osc) => { osc.connect(object) } );
+    this.oscillators.forEach( osc => osc.connect(object) );
   }
 
-  start() {
-    this.oscillators.forEach( (osc) => { osc.start() } );
+  start( time ) {
+    if(!time) time = this.context.currentTime;
+    // this.oscillators.forEach( osc => osc.start(time) );
+    this.oscillators[0].start();
+    this.oscillators[1].start();
   }
 
   render() {
